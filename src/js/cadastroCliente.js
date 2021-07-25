2230447e5a0a1a1d2f1f7ac6910de797cfd0f260
Arielle Silva <arielle95@gmail.com>
2021-07-25 05:27:42 -0300

    primeiro commit arquivos principais

---
+++ src/js/cadastroCliente.js
@@ -0,0 +1,93 @@
+const button1 = document.getElementById('confirmarCadastroCliente');
+const clientes = [];
+const buttonVoltarCliente = document.getElementById('voltarCadCliente');
+
+class Pessoa {
+    constructor(nome, email, rua, numeroResidencia, bairro, cidade, estado, telefone){
+        this.nome = nome;
+        this.email = email;
+        this.rua = rua;
+        this.numeroResidencia = numeroResidencia;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.telefone = telefone;
+    }
+
+}
+
+function confirmaCadastro(e) {
+    e.preventDefault();
+    let nome = document.getElementById('nomeCliente').value;
+    let email = document.getElementById('email').value;
+    let rua = document.getElementById('rua').value;
+    let numeroResidencia = document.getElementById('numero').value;
+    let bairro = document.getElementById('bairro').value;
+    let cidade = document.getElementById('cidade').value;
+    let estado = document.getElementById('estado').value;
+    let telefone = document.getElementById('telefone').value;
+    
+    if (nome != "" && email != "" && rua != "" && numeroResidencia != "" && bairro != "" 
+    && cidade != "" && estado != "" && telefone != ""){
+        let convertNome = JSON.stringify(nome);
+        localStorage.setItem('nomeCliente', convertNome);
+        
+        let convertEmail = JSON.stringify(email);
+        localStorage.setItem('email', convertEmail);
+
+        let convertRua = JSON.stringify(rua);
+        localStorage.setItem('rua', convertRua);
+        
+        let convertNumResidencia = JSON.stringify(numeroResidencia);
+        localStorage.setItem('numResidencia', convertNumResidencia);
+
+        let convertBairro = JSON.stringify(bairro);
+        localStorage.setItem('bairro', convertBairro);
+
+        let convertCidade = JSON.stringify(cidade);
+        localStorage.setItem('cidade', convertCidade);
+
+        let convertEstado = JSON.stringify(estado);
+        localStorage.setItem('estado', convertEstado);
+
+        let convertTelefone = JSON.stringify(telefone);
+        localStorage.setItem('telefone', convertTelefone);
+
+        let novoCliente = new Pessoa(convertNome, convertEmail, convertRua, convertNumResidencia, 
+            convertBairro, convertCidade, convertEstado, convertTelefone);
+        
+        clientes.push(novoCliente);
+        
+        localStorage.setItem('clientes', JSON.stringify(clientes));
+
+        document.getElementById('formCliente').reset();
+
+    }else{
+        if(nome == ""){
+            alert("Campo NOME COMPLETO não preenchido!")
+        }else if(email == ""){
+            alert("Campo E-MAIL não preenchido!")
+        }else if(rua == ""){
+            alert("Campo RUA não preenchido!")
+        }else if(numeroResidencia == ""){
+            alert("Campo NÚMERO DA RESIDÊNCIA não preenchido!")
+        }else if(bairro == ""){
+            alert("Campo BAIRRO não preenchido!")
+        }else if(cidade == ""){
+            alert("Campo CIDADE não preenchido!")
+        }else if(estado == ""){
+            alert("Campo ESTADO não preenchido!")
+        }else if(telefone == ""){
+            alert("Campo TELEFONE não preenchido!")
+        }
+    }
+}
+
+function voltarCliente(e){
+    e.preventDefault();
+    window.location.href = '../pages/index.html';
+}
+
+button1.addEventListener('click', confirmaCadastro);
+
+buttonVoltarCliente.addEventListener('click', voltarCliente);
---
+++ src/js/cadastroProduto.js
@@ -0,0 +1,81 @@
+const button1 = document.getElementById('confirmarCadastroProduto');
+const produtos = [];
+const buttonVoltarCliente = document.getElementById('voltarCadProduto');
+
+
+class Produto {
+    constructor(nome, codigoBarras, descricao, tipo, preco, qtdEstoque){
+        this.nome = nome;
+        this.codigoBarras = codigoBarras;
+        this.descricao = descricao;
+        this.tipo = tipo;
+        this.preco = preco;
+        this.qtdEstoque = qtdEstoque;
+        
+    }
+
+}
+
+function confirmaCadastro(e) {
+    e.preventDefault();
+    let nome = document.getElementById('nomeProduto').value;
+    let codigoBarras = document.getElementById('codigoBarras').value;
+    let descricao = document.getElementById('descricaoProduto').value;
+    let tipo = document.getElementById('tipoProduto').value;
+    let preco = document.getElementById('precoProduto').value;
+    let qtdEstoque = document.getElementById('qtdProduto').value;
+    
+    if (nome != "" && codigoBarras != "" && descricao != "" && tipo != "" 
+    && preco != "" && qtdEstoque != ""){
+        let convertNome = JSON.stringify(nome);
+        localStorage.setItem('nomeProduto', convertNome);
+        
+        let convertCodigoBarras = JSON.stringify(codigoBarras);
+        localStorage.setItem('codigoBarras', convertCodigoBarras);
+
+        let convertDescricao = JSON.stringify(descricao);
+        localStorage.setItem('descricao', convertDescricao);
+        
+        let convertTipo = JSON.stringify(tipo);
+        localStorage.setItem('tipo', convertTipo);
+
+        let convertPreco = JSON.stringify(preco);
+        localStorage.setItem('preco', convertPreco);
+
+        let convertQtdEstoque = JSON.stringify(qtdEstoque);
+        localStorage.setItem('qtdEstoque', convertQtdEstoque);
+
+
+        let novoProduto = new Produto(convertNome, convertCodigoBarras, convertDescricao,
+            convertTipo, convertPreco, convertQtdEstoque);
+        produtos.push(novoProduto);
+        
+        localStorage.setItem('produtos', JSON.stringify(produtos));
+        
+        document.getElementById('formProduto').reset();
+
+    }else{
+        if(nome == ""){
+            alert("Campo NOME PRODUTO não preenchido!")
+        }else if(codigoBarras == ""){
+            alert("Campo CÓDIGO DE BARRAS não preenchido!")
+        }else if(descricao == ""){
+            alert("Campo DESCRIÇÃO DO PRODUTO não preenchido!")
+        }else if(tipo == ""){
+            alert("Campo TIPO DO PRODUTO não preenchido!")
+        }else if(preco == ""){
+            alert("Campo PREÇO não preenchido!")
+        }else if(qtdEstoque == ""){
+            alert("Campo QUANTIDADE DO PRODUTO EM ESTOQUE não preenchido!")
+        }
+    }
+}
+
+button1.addEventListener('click', confirmaCadastro);
+
+function voltarCliente(e){
+    e.preventDefault();
+    window.location.href = '../pages/index.html';
+}
+
+buttonVoltarCliente.addEventListener('click', voltarCliente);
